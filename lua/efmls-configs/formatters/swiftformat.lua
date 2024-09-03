245ebf2150189c474336ecd8a30a5d94c889128c
toofonwang <toofonwang@futunn.com>
2024-09-03 21:31:55 +0800

    modify swiftformat config

--- lua/efmls-configs/formatters/swiftformat.lua
+++ lua/efmls-configs/formatters/swiftformat.lua
@@ -6,7 +6,7 @@ local fs = require('efmls-configs.fs')
 
 local formatter = 'swiftformat'
 local args = string.format('--baseconfig %s', vim.g.tool_config_path .. "/swiftformat/swiftformat.config")
-local command = string.format('%s stdin --output stdout --quiet --stdinpath "${INPUT} %s"', fs.executable(formatter), args)
+local command = string.format('%s stdin --output stdout --quiet --stdinpath "${INPUT}" %s', fs.executable(formatter), args)
 
 return {
   formatCommand = command,
